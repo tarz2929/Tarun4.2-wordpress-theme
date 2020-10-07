0db707eb87e30f53fd7ff505d35860a951ce0567
Warren Uhrich <hello@warren.codes>
2020-10-07 12:00:06 -0600

    Add category list/link output to single.php

--- single.php
+++ single.php
@@ -8,9 +8,31 @@
     <p>
       <strong>Article Published On:</strong>
       <time datetime="<?php echo get_the_date( DATE_ATOM ); ?>">
-        <?php echo get_the_date(); // WP function; gets the date of posting. ?>
+        <?php echo get_the_date(); ?>
       </time>
     </p>
+    <h3>Post Categories</h3>
+    <ul>
+      <?php
+        // Get an array of category ID numbers for THIS post. 
+        $categories = wp_get_post_categories( get_the_id() );
+        // Loop through the post categories...
+        foreach ( $categories as $catID )
+        { // Retrieve the WP_Term object for this ID.
+          $category = get_category( $catID );
+          // Retrive the current category's URL (link.)
+          $categoryURL = get_term_link( $category );
+          // Output the formatted list item (LI.)
+          ?>
+            <li>
+              <a href="<?php echo $categoryURL; ?>">
+                <?php echo $category->name; // Property from WP_Term ?>
+              </a>
+            </li>
+          <?php
+        }
+      ?>
+    </ul>
     <h3>Post Content</h3>
     <?php the_content(); // Displays post contents. ?>
 </article>
