632a0abf75ea0a5e7a16ff8e7b0898a1c420b32f
Warren Uhrich <hello@warren.codes>
2020-10-07 13:30:28 -0600

    Add footer menu.

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
+  <nav>
+    <h2>Footer Navigation</h2>
+    <?php
+      wp_nav_menu( 
+        array( // This name is set in: functions.php
+          'theme_location' => 'footer_menu'
+        )
+      );
+    ?>
+  </nav>
   <footer>
     <h2><?php bloginfo( 'title' ); ?> Footer</h2>
     <p>
