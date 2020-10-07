0539b512046c6663fce0501fe4dd303cb9b477c3
Warren Uhrich <hello@warren.codes>
2020-10-07 11:20:53 -0600

    Add 404.php

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<h2>Error: 404</h2>
+
+<p>
+  The page or post you requested was not found.
+  <a href="<?php echo site_url(); ?>">
+    Click here to return to the homepage
+  </a>
+  and try again. Thank you.
+</p>
+
+<?php get_footer();
