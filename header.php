8b409746875cbb305fe390b11d5e279e3ac076e1
Warren Uhrich <hello@warren.codes>
2020-10-07 11:07:26 -0600

    Add header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo( 'title' ); ?></title>
+  <?php
+    // This will output any scripts, styles, meta,
+    // and more that our theme / plugin need.    
+    wp_head();
+  ?>
+</head>
+<body>
+  <h1><?php bloginfo( 'title' ); ?></h1>
+  
