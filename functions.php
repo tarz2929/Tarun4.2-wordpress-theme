f37237df473c074b33e8c94a8941509bb10f4bd9
Warren Uhrich <hello@warren.codes>
2020-10-07 13:50:37 -0600

    Add enqueue for JS script.

--- functions.php
+++ functions.php
@@ -15,7 +15,18 @@ register_nav_menus( // We must register nav locations if we want the admin to be
  */
 add_action( 'wp_enqueue_scripts', function ()
 {
-
-  
+  // Enqueue our JavaScript!
+  wp_enqueue_script(
+    // An ID / name for our script.
+    'techcareers-theme-scripts',
+    // The "public" path to the script file.
+    get_theme_file_uri( '/assets/js/scripts.js' ),
+    // Dependencies (jQuery, Bootstrap, etc.)
+    array(),
+    // Version number of the file (based on time to fight browser cache!)
+    strftime( get_theme_file_uri( '/assets/js/scripts.js' ) ),
+    // Should we output this script in the wp_footer()?
+    FALSE
+  );
 
 } );
